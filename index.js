38e1b3f74d708977c18d00f76470d35440094c6c
anuragpeshne <anurag.peshne@gmail.com>
2017-09-29 15:19:09 -0400

    commandq and readers implemented, 1st working version

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const STATIC_PORT = 8080
-const SOCKET_PORT = 8081
+const STATIC_PORT = 8080;
+const SOCKET_PORT = 8081;
+const ANIM_SPEED  = 200;
 
+commandq = [];
+inpEof = false;
+
+var stdin = process.openStdin();
+stdin.addListener("data", function(d) {
+  var inp = d.toString();
+  console.log(inp.trim());
+  var lines = inp.split('\n');
+  for (var lineNo = 0; lineNo < lines.length; lineNo++) {
+    var plottyRegex = /.*<plotty:(.*)>.*/g;
+    var plottyCommand = plottyRegex.exec(lines[lineNo]);
+
+    if (plottyCommand != null) {
+      plottyCommand = plottyCommand[1]
+      command = plottyCommand.split(",");
+      command = command.map(function(e) {return e.trim()});
+      commandq.push(command);
+    }
+  }
+}).addListener("end", function() {
+  inpEof = true;
+});
 // create static web server
 var connect = require('connect');
 var serveStatic = require('serve-static');
@@ -19,11 +42,12 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', message);
   });
 
-
-  var stdin = process.openStdin();
-  stdin.addListener("data", function(d) {
-    console.log("you entered: [" +
-                d.toString().trim() + "]");
-    ws.send(d.toString().trim());
-  });
+  setInterval(function() {
+    if (commandq.length > 0) {
+      ws.send(JSON.stringify(commandq[0]));
+      commandq.splice(0, 1);
+    } else if (inpEof) {
+      process.exit();
+    }
+  }, ANIM_SPEED);
 });
